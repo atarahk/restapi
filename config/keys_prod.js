4f7e3f67b19d0e9fb0c7a436334eeb17e3739740
Atarah <atarahplayground@gmail.com>
2019-05-27 13:27:54 +0800

    3

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,10 @@
+// module.exports = {
+//   mongoURI: process.env.MONGO_URI,
+//   secretOrKey: process.env.SECRET_OR_KEY
+// };
+
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.SECRET_OR_KEY
-};
+  mongoURI:
+    'mongodb+srv://ata:ata@midmaytestgooglecloud-byzgf.gcp.mongodb.net/test?retryWrites=true',
+  secretOrKey: 'SECRET'
+}
