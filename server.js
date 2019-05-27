df7187f3f3d7f664c81ac25496cc50c667979a4a
Atarah <atarahplayground@gmail.com>
2019-05-27 08:55:43 +0800

    restapi

--- server.js
+++ server.js
@@ -14,22 +14,14 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-// // DB Config
-// const db = require('./config/keys').mongoURI
+// DB Config
+const db = require('./config/keys').mongoURI
 
-// // Connect to MongoDB
-// mongoose
-//   .connect(db, { useNewUrlParser: true })
-//   .then(() => console.log('MongoDB Connected'))
-//   .catch(err => console.log(err))
-
-//MongoURI is temporary, will expiry soon
-mongoose.connect(
-  'mongodb+srv://ata:ata@midmaytestgooglecloud-byzgf.gcp.mongodb.net/test?retryWrites=true',
-  {
-    useNewUrlParser: true
-  }
-)
+// Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err))
 
 // Passport middleware
 app.use(passport.initialize())
